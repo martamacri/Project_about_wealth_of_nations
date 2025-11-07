0c5f5fc2a0317b95d2d0a7e5b32d4683fc7cd3db
mmacri12 <martamacri03@gmail.com>
2025-11-07 22:46:24 +0100

    Create data_loader.py
    I modify the script folder and then create the “main” file.

---
+++ scripts/data_loader.py
@@ -0,0 +1,100 @@
+"""
+Modulo per scaricare e gestire dati economici dalla Banca Mondiale.
+"""
+import wbgapi as wb
+import pandas as pd
+
+# per vedere tutti gli indicatori della libreria
+# for indicator in wb.series.list():
+#    print(indicator['id'], "-", indicator['value'])
+# sono tanti quindi ne scegli solo alcuni
+# per vedere tutti i paesi che ci sono all'interno della libreria
+# for country in wb.economy.list():
+#    print(country['id'], "-", country['value'])
+# ma sono tanti anche in questo caso ne scegli solo alcuni
+
+# Scatico i file csv indicatore per indicatore
+
+# Indicator 1: GDP per capita, current US$
+indicator1 = 'NY.GDP.PCAP.CD'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df1 = wb.data.DataFrame(indicator1, countries, time=years)
+df1 = df1.T
+df1.index.name = 'Year'
+df1 = df1.apply(pd.to_numeric, errors='coerce')
+print(df1.head())
+df1.to_csv('gdp_per_capita_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: gdp_per_capita_1995_2024.csv")
+
+# Indicator 2: GDP
+indicator2 = 'NY.GDP.MKTP.CD'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df2 = wb.data.DataFrame(indicator2, countries, time=years)
+df2 = df2.T
+df2.index.name = 'Year'
+df2 = df2.apply(pd.to_numeric, errors='coerce')
+print(df2.head())
+df2.to_csv('gdp_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: gdp_1995_2024.csv")
+
+# Indicator 3: Life expectancy
+indicator3 = 'SP.DYN.LE00.IN'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df3 = wb.data.DataFrame(indicator3, countries, time=years)
+df3 = df3.T
+df3.index.name = 'Year'
+df3 = df3.apply(pd.to_numeric, errors='coerce')
+print(df3.head())
+df3.to_csv('life_expectacy_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: life_expectacy_1995_2024.csv")
+
+# Indicator 4: Health expenditure
+indicator4 = 'SH.XPD.CHEX.PC.CD'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df4 = wb.data.DataFrame(indicator4, countries, time=years)
+df4 = df4.T
+df4.index.name = 'Year'
+df4 = df4.apply(pd.to_numeric, errors='coerce')
+print(df4.head())
+df4.to_csv('health_expenditure_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: health_expenditure_1995_2024.csv")
+
+# Indicator 5: Infant mortality
+indicator5 = 'SH.DYN.MORT'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df5 = wb.data.DataFrame(indicator5, countries, time=years)
+df5 = df5.T
+df5.index.name = 'Year'
+df5 = df5.apply(pd.to_numeric, errors='coerce')
+print(df5.head())
+df5.to_csv('infant_mortality_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: infant_mortality_1995_2024.csv")
+
+# Indicator 6: Unemployment
+indicator6 = 'SL.UEM.TOTL.ZS'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df6 = wb.data.DataFrame(indicator6, countries, time=years)
+df6 = df6.T
+df6.index.name = 'Year'
+df6 = df6.apply(pd.to_numeric, errors='coerce')
+print(df6.head())
+df6.to_csv('unemployment_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: unemployment_1995_2024.csv")
+
+# Indicator 7: Population growth
+indicator7 = 'SP.POP.GROW'
+countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
+years = range(1995, 2025)
+df7 = wb.data.DataFrame(indicator7, countries, time=years)
+df7 = df7.T
+df7.index.name = 'Year'
+df7 = df7.apply(pd.to_numeric, errors='coerce')
+print(df7.head())
+df7.to_csv('population_growth_1995_2024.csv', float_format='%.2f')
+print("\nSalvato: population_growth_1995_2024.csv")
