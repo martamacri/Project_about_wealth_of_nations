968791151ff10f267f973ce957042f771984466d
mmacri12 <martamacri03@gmail.com>
2025-11-10 16:48:02 +0100

    Update wellness_indicator.py
    delete the output

--- scripts/wellness_indicator.py
+++ scripts/wellness_indicator.py
@@ -1,22 +1,6 @@
 """
 indicatore di benessere
 """
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from formatting import prepare_df
-sns.set(style="whitegrid")
-
-df1 = pd.read_csv('data/gdp_per_capita_1995_2024.csv')
-df3 = pd.read_csv('data/life_expectacy_1995_2023.csv')
-df4 = pd.read_csv('data/health_expenditure_2000_2022.csv')
-df5 = pd.read_csv('data/infant_mortality_1995_2023.csv')
-
-pil = prepare_df(df1)
-life = prepare_df(df3)
-health = prepare_df(df4)
-infant_mort = prepare_df(df5)
-
 #indice sintetico di benessere che riassume tutti gli indici di benessere
 #(aspettativa di vita, spesa sanitaria, mortalità infantile)
 #normalizzo i dati che devono avere tutti lo stesso ordine
@@ -48,38 +32,3 @@ def compute_wellbeing_index(life_df, health_df, infant_df):
     wellbeing_index_df = (life_norm + health_norm + infant_norm) / 3
 
     return wellbeing_index_df, countries_df
-
-# Calcolo dell'indice
-wellbeing_index, countries = compute_wellbeing_index(life, health, infant_mort)
-
-# Evoluzione media nel tempo
-plt.figure(figsize=(12, 6))
-wellbeing_index.mean(axis=1).plot()
-plt.title("Summary index of average well-being over time")
-plt.xlabel("Year")
-plt.ylabel("Well-being index")
-plt.grid(True)
-plt.show()
-
-# Classifica dei paesi per indice medio
-mean_wellbeing = wellbeing_index.mean(axis=0)
-rank = mean_wellbeing.sort_values(ascending=False)
-print("Ranking of countries by average well-being index:")
-print(rank)
-
-# Confronto con PIL medio
-common_years = wellbeing_index.index.intersection(pil.index)
-gdp_mean = pil.loc[common_years, countries].mean()
-plt.figure(figsize=(10, 6))
-plt.scatter(gdp_mean, mean_wellbeing)
-for country in countries:
-    plt.text(gdp_mean[country], mean_wellbeing[country], country, fontsize=8)
-plt.xlabel("Average GDP per capita")
-plt.ylabel("Average well-being index")
-plt.title("Relationship between GDP per capita and well-being index")
-plt.grid(True)
-plt.show()
-
-# Correlazione
-corr = gdp_mean.corr(mean_wellbeing)
-print(f"Pearson correlation between average GDP per capita and well-being index: {corr:.3f}")
