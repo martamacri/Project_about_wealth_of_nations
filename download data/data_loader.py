247039c3d5fd7f56149ff47c2632ad66b86dc84f
mmacri12 <martamacri03@gmail.com>
2025-11-28 23:35:23 +0100

    Update data_loader.py
    correction of doc string

--- download data/data_loader.py
+++ download data/data_loader.py
@@ -15,22 +15,21 @@ import pandas as pd
 
 # I download the CSV files indicator by indicator.
 
-# Indicator 1: GDP per capita, current US$
-GPD_PC = 'NY.GDP.PCAP.CD'
 countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
 years = range(1995, 2025)
+
+# Indicator 1: GDP per capita, current US$
+GPD_PC = 'NY.GDP.PCAP.CD'
 df1 = wb.data.DataFrame(GPD_PC, countries, time=years)
-df1 = df1.T
+df1 = df1.T #transpose dataset
 df1.index.name = 'Year'
-df1 = df1.apply(pd.to_numeric, errors='coerce')
-print(df1.head())
+df1 = df1.apply(pd.to_numeric, errors='coerce') # converts the values in the dataset into numerical values
+print(df1.head()) # 5 rows
 df1.to_csv('gdp_per_capita_1995_2024.csv', float_format='%.2f')
 print("\nSaved: gdp_per_capita_1995_2024.csv")
 
 # Indicator 2: GDP
 GPD = 'NY.GDP.MKTP.CD'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df2 = wb.data.DataFrame(GPD, countries, time=years)
 df2 = df2.T
 df2.index.name = 'Year'
@@ -41,8 +40,6 @@ print("\nSaved: gdp_1995_2024.csv")
 
 # Indicator 3: Life expectancy
 LIFE = 'SP.DYN.LE00.IN'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df3 = wb.data.DataFrame(LIFE, countries, time=years)
 df3 = df3.T
 df3.index.name = 'Year'
@@ -53,8 +50,6 @@ print("\nSaved: life_expectacy_1995_2024.csv")
 
 # Indicator 4: Health expenditure
 HEALTH = 'SH.XPD.CHEX.PC.CD'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df4 = wb.data.DataFrame(HEALTH, countries, time=years)
 df4 = df4.T
 df4.index.name = 'Year'
@@ -65,8 +60,6 @@ print("\nSaved: health_expenditure_1995_2024.csv")
 
 # Indicator 5: Infant mortality
 INFANT = 'SH.DYN.MORT'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df5 = wb.data.DataFrame(INFANT, countries, time=years)
 df5 = df5.T
 df5.index.name = 'Year'
@@ -77,8 +70,6 @@ print("\nSaved: infant_mortality_1995_2024.csv")
 
 # Indicator 6: Unemployment
 UNEMP = 'SL.UEM.TOTL.ZS'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df6 = wb.data.DataFrame(UNEMP, countries, time=years)
 df6 = df6.T
 df6.index.name = 'Year'
@@ -89,8 +80,6 @@ print("\nSaved: unemployment_1995_2024.csv")
 
 # Indicator 7: Population growth
 POP = 'SP.POP.GROW'
-countries = ['ITA', 'ESP', 'DEU', 'SWE', 'USA', 'CAN', 'BRA', 'CHL', 'ZAF', 'NGA']
-years = range(1995, 2025)
 df7 = wb.data.DataFrame(POP, countries, time=years)
 df7 = df7.T
 df7.index.name = 'Year'
