3ced43ec254257a11bd19db5466d9b4a7eaed673
mmacri12 <martamacri03@gmail.com>
2025-11-28 23:35:40 +0100

    Update correlations.py
    correction of doc string

--- scripts/correlations.py
+++ scripts/correlations.py
@@ -6,13 +6,16 @@ import pandas as pd
 # Function to calculate correlations by country
 def compute_country_correlations(df_x, df_y, name_x="X", name_y="Y"):
     """
-    Total correlations
+    Compute Pearson correlations between two datasets for each country
+    Return a summary table with correlations for all countries
+    Parameters: df x, df y. Two datasets about two different indicators
+    Returns: country, x, y, correlation
     """
     results = []
-    for countries in df_x.columns.intersection(df_y.columns):
+    for countries in df_x.columns.intersection(df_y.columns): # Loop over countries
         x_vals = df_x[countries]
         y_vals = df_y[countries]
-        valid_idx = x_vals.notna() & y_vals.notna()
+        valid_idx = x_vals.notna() & y_vals.notna() #identify valid values
         corr = x_vals[valid_idx].corr(y_vals[valid_idx]) if valid_idx.sum() > 1 else None
         results.append({
             'Country': countries,
@@ -25,11 +28,15 @@ def compute_country_correlations(df_x, df_y, name_x="X", name_y="Y"):
 # Function to calculate rolling correlation for all countries
 def compute_rolling_correlation(df_x, df_y, window=15):
     """
-    Correlation over time
+    Compute rolling Pearson correlation over time for multiple countries
+    Useful to see how correlations evolve over time
+    Window: numbers of years
+    Returns dataset where each cell contains the rolling correlation at that year for the country
     """
-    rolling = pd.DataFrame(index=df_x.index)
-    for countries in df_x.columns.intersection(df_y.columns):
+    rolling = pd.DataFrame(index=df_x.index) # Create an empty DataFrame
+    for countries in df_x.columns.intersection(df_y.columns): # Loop over countries
         x = df_x[countries]
         y = df_y[countries]
         rolling[countries] = x.rolling(window=window, min_periods=3).corr(y)
+        # min_periods=3 ensures at least 3 valid observations are needed for correlation
     return rolling
